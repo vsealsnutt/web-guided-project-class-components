eb02cb48741a98798554a6ab2c2c144bdf239650
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-17 11:13:55 -0500

    followed along with the guided video to create class components and create event handlers to make the grocery list more interactive and functional

--- followAlong/src/index.js
+++ followAlong/src/index.js
@@ -39,16 +39,49 @@ const groceries = [
 ];
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      groceries: groceries
+    }
+  }
+
   // Class methods to update state
+  addItem = (e, item) => {
+    e.preventDefaults();
+    const newItem = {
+      name: item,
+      id: Date.now(),
+      purchased: false
+    } 
+    this.setState({...this.state, groceries: [...this.state.groceries, newItem]});
+  }
+
+  toggleItem = itemId => {
+    console.log(itemId);
+    this.setState({...this.state, groceries: this.state.groceries.map(item => {
+      if(item.id === itemId) {
+        return {...item, purchased: !item.purchased}
+      }
+      return item;
+    })})
+  }
+
+  clearPurchased = () => {
+    this.setState({...this.state, groceries: this.state.groceries.filter(item => {
+      if (!item.purchased) return item;
+    })})
+  }
+
   render() {
     return (
       <div className="App">
         <div className="header">
            <h1>Shopping List</h1>
-           <ListForm />
+           <ListForm addItem={this.addItem} />
          </div>
-        <GroceryList groceries={groceries} />
-        <button className="clear-btn">Clear Purchased</button>
+        <GroceryList toggleItem={this.toggleItem} groceries={this.state.groceries} />
+        <button onClick={this.clearPurchased} className="clear-btn">Clear Purchased</button>
        </div>
     );
   }
